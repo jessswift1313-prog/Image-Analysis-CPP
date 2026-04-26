8858778be7da725e87bff2d576e0ff9e115e3c02
Sichao Jing <jessswift1313@gmail.com>
2026-04-26 18:12:30 +0200

    delete 1st version

--- CODES/open_ppm.py
+++
@@ -1,8 +0,0 @@
-import netpbmfile
-import numpy
-from PIL import Image
-def open_ppm(file):
-    a = netpbmfile.imread(file)
-    if file.endswith(".pbm"):
-        a = numpy.logical_not(a)
-    return Image.fromarray(a)
